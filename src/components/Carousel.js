e54ec05c3d9dbb16f78ee9c80581fc8b68d26acc
harish <harrych_1@yahoo.com>
2023-02-03 00:31:07 +0530

    add-images

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,7 +5,7 @@ function Carousel() {
     <div><div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
     <div className="carousel-inner" id='carousel'>
       <div className="carousel-item active">
-        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fget.pxhere.com%2Fphoto%2Focean-restaurant-dish-meal-food-seafood-fish-cuisine-invertebrate-shrimp-lobster-asian-food-animal-source-foods-spiny-lobster-1237990.jpg&f=1&nofb=1&ipt=4593d5fb184094cc7bf517f06150e68eba8140f8f14613086c312c0ab4d6733b&ipo=images" class="d-block w-100"  style={{brightness:"(30%)"}}  alt="..."/>
+        <img src="https://github.com/HarishChavre/Seafood-App/blob/master/IMAGES/Fish-Biryani-8-1536x1024.jpg?raw=true" class="d-block w-100"  style={{brightness:"(30%)"}}  alt="..."/>
       </div>
       <div className="carousel-item">
         <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.VLqybyxzgiBE68_OpeQrcwHaEL%26pid%3DApi&f=1&ipt=1ec061e1a4043b11f451f8d70b6e80b2a8341d00f89a8148af875ad6183349a6&ipo=images" class="d-block w-100" style={{brightness:"(30%)"}} alt="..."/>
